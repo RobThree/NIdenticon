7bf8509744eabbd6c4a533bb430f8007801e7911
RobThree <rob@devcorner.nl>
2014-01-16 01:13:22 +0100

    * Added (simple) example website

---
+++ TestWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace TestWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ TestWeb/Controllers/NIdenticonController.cs
@@ -0,0 +1,24 @@
+﻿using Devcorner.NIdenticon;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class NIdenticonController : Controller
+    {
+        [Route("nidenticon/{dimension:range(10,500)=50}/{text?}")]
+        public ActionResult Index(int dimension, string text)
+        {
+            text = text ?? HttpContext.Request.UserHostAddress;
+            var ic = new IdenticonGenerator();
+            var bitmap = ic.Create(text, dimension, dimension);
+
+            var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Png);
+
+            return File(ms.ToArray(), "image/png");
+        }
+    }
+}
