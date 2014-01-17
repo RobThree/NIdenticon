f99ddfcdaf7c9565d7fd7857e1163f4a2f922a99
RobThree <rob@devcorner.nl>
2014-01-17 20:17:20 +0100

    * Added missing file

---
+++ NIdenticon/BlockGenerators/PieGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Devcorner.NIdenticon.BlockGenerators
+{
+    public class PieGenerator : BlockGenerator
+    {
+        public PieGenerator(int weight)
+            : base(weight) { }
+
+        public override void Draw(Graphics g, Rectangle r, Brush bg, Brush fg, uint seed, bool fliphorizontal)
+        {
+            switch ((seed + (fliphorizontal ? 2 : 0)) % 4)
+            {
+                case 0: //  ◞
+                    g.FillPie(fg, new Rectangle(r.Left - r.Width, r.Top - r.Height, r.Width * 2, r.Height * 2), 0, 90);
+                    break;
+                case 1: //  ◜
+                    g.FillPie(fg, new Rectangle(r.Left, r.Top, r.Width * 2, r.Height * 2), -90, -90);
+                    break;
+                case 2: //  ◟
+                    g.FillPie(fg, new Rectangle(r.Left, r.Top - r.Height, r.Width * 2, r.Height * 2), 90, 90);
+                    break;
+                case 3: //  ◝
+                default:
+                    g.FillPie(fg, new Rectangle(r.Left - r.Width, r.Top, r.Width * 2, r.Height * 2), 0, -90);
+                    break;
+            }
+        }
+    }
+}
